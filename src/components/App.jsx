38c9486d87e631dcb36b5e4d9a83e926eb4c0890
dima-zharyy <zharyydimka@gmail.com>
2022-06-21 22:48:25 +0300

    added-starter-files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
+import Profile from './Profile';
+import user from './user.json';
+
+// const {
+//   username,
+//   tag,
+//   location,
+//   avatar,
+//   stats: { followers, views, likes },
+// } = user;
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={10}
+        views={15}
+        likes={30}
+      />
     </div>
   );
 };
